8037d8f8f73fdf73591e677e6b41c134e4b6658c
Heeyun0724 <heoheeyun0724@naver.com>
2024-02-06 15:32:52 +0900

    bluetooth fix

--- lib/puzzle_page.dart
+++ lib/puzzle_page.dart
@@ -2,7 +2,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+//import 'package:fluttertoast/fluttertoast.dart';
 
 class PuzzlePage extends StatefulWidget {
   final String imagePath;
@@ -19,7 +19,9 @@ class _PuzzlePageState extends State<PuzzlePage> {
   FlutterBlue flutterBlue = FlutterBlue.instance;
   List<BluetoothDevice> devices = [];
 
+
   Future<Size> _getImageSize(Image image) async {
+    // 이미지의 크기를 비동기적으로 가져오는 함수
     final Completer<Size> completer = Completer<Size>();
     image.image.resolve(const ImageConfiguration()).addListener(
       ImageStreamListener(
@@ -67,24 +69,40 @@ class _PuzzlePageState extends State<PuzzlePage> {
                             path: 'images/puzzle/block${index + 1}.png',
                             position: Offset.zero,
                             size: snapshot.data!,
+                            blockIndex: index + 1,
                           ),
                           feedback: image,
                           child: image,
                           onDragEnd: (details) {
-                            final RenderBox targetBox =
-                            _targetKey.currentContext!.findRenderObject() as RenderBox;
+                            final RenderBox targetBox = _targetKey.currentContext!.findRenderObject() as RenderBox;
                             final targetPosition = targetBox.globalToLocal(details.offset);
 
-                            if (targetBox.size.contains(targetPosition)) {
-                              setState(() {
-                                droppedImages.add(DraggableImage(
-                                  name: 'images/puzzle/block${index + 1}',
-                                  path: 'images/puzzle/block${index + 1}.png',
-                                  position: targetPosition,
-                                  size: snapshot.data!,
-                                ));
-                              });
-                            }
+                            setState(() {
+                              final newImage = DraggableImage(
+                                name: 'images/puzzle/block${index + 1}',
+                                path: 'images/puzzle/block${index + 1}.png',
+                                position: targetPosition,
+                                size: snapshot.data!,
+                                blockIndex: index + 1,
+                              );
+
+                              // 첫 번째 블록이 드랍되지 않은 경우 위치를 조정
+                              if (droppedImages.isEmpty) {
+                                newImage.position = Offset(
+                                  targetPosition.dx - snapshot.data!.width / 2,
+                                  targetPosition.dy - snapshot.data!.height / 2,
+                                );
+                              } else {
+                                // 첫 번째 블록 이후의 블록은 가장 가까운 스냅 포인트에 맞춰 붙도록 설정
+                                final nearestImage = getNearestImage(newImage);
+                                if (nearestImage != null) {
+                                  final snappedPosition = getSnappedPosition(newImage, nearestImage);
+                                  newImage.position = snappedPosition;
+                                }
+                              }
+
+                              droppedImages.add(newImage);
+                            });
                           },
                         );
                       } else {
@@ -127,6 +145,53 @@ class _PuzzlePageState extends State<PuzzlePage> {
     );
   }
 
+  DraggableImage? getNearestImage(DraggableImage newImage) {
+    DraggableImage? nearestImage;
+
+    double minDistance = double.infinity;
+
+    for (var droppedImage in droppedImages) {
+      final distance = (newImage.position - droppedImage.position).distance;
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestImage = droppedImage;
+      }
+    }
+
+    return nearestImage;
+  }
+
+  Offset getSnappedPosition(DraggableImage newImage, DraggableImage nearestImage) {
+    final snapPoints = {
+      1: {'left': Offset(0, 50), 'right': Offset(116, 50)},
+      2: {'left': Offset(116, 50), 'right': Offset(216, 50)},
+      3: {'left': Offset(216, 50), 'right': Offset(316, 50)},
+    };
+
+    final leftSnapPoint = snapPoints[nearestImage.blockIndex]!['left']!;
+    final rightSnapPoint = snapPoints[nearestImage.blockIndex]!['right']!;
+
+    final newImageCenter =
+    Offset(newImage.position.dx + newImage.size.width / 2,
+        newImage.position.dy + newImage.size.height / 2);
+
+    final nearestImageCenter =
+    Offset(nearestImage.position.dx + nearestImage.size.width / 2,
+        nearestImage.position.dy + nearestImage.size.height / 2);
+
+    final leftDistance = (newImageCenter - leftSnapPoint).distance;
+    final rightDistance = (newImageCenter - rightSnapPoint).distance;
+
+    final snappedPoint =
+    leftDistance < rightDistance ? leftSnapPoint : rightSnapPoint;
+
+    return Offset(
+      snappedPoint.dx - newImage.size.width / 2,
+      snappedPoint.dy - newImage.size.height / 2,
+    );
+  }
+
+
   void startBluetoothScan() {
     flutterBlue.startScan(timeout: Duration(seconds: 10));
 
@@ -155,16 +220,25 @@ class _PuzzlePageState extends State<PuzzlePage> {
                     subtitle: Text(devices[index].id.toString()),
                     onTap: () {
                       devices[index].connect().then((_) {
-                        Fluttertoast.showToast(
-                            msg: "Connected to ${devices[index].name}",
-                            toastLength: Toast.LENGTH_SHORT,
-                            gravity: ToastGravity.BOTTOM,
-                            timeInSecForIosWeb: 1,
-                            backgroundColor: Colors.green,
-                            textColor: Colors.white,
-                            fontSize: 16.0
-                        );
                         Navigator.of(context).pop();
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Connection Successful'),
+                              content: Text(
+                                  "Connected to ${devices[index].name}"),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text("Close"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
                       });
                     },
                   );
@@ -191,6 +265,13 @@ class DraggableImage {
   final String path;
   Offset position;
   Size size;
+  int blockIndex; // 블록의 인덱스 추가
 
-  DraggableImage({required this.name, required this.path, required this.position, required this.size});
+  DraggableImage({
+    required this.name,
+    required this.path,
+    required this.position,
+    required this.size,
+    required this.blockIndex,
+  });
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_blue
-import flutter_blue_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
-  FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
 }
