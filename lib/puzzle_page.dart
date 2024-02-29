6c497b22430edd96fe9e570a265c37f54d01cfd2
jisally <jisally@naver.com>
2024-02-29 13:58:57 +0900

    0229 지우씨거랑 합침

--- lib/puzzle_page.dart
+++ lib/puzzle_page.dart
@@ -453,18 +453,6 @@ class _PuzzlePageState extends State<PuzzlePage> {
                         BluetoothHelper.sendData(connected_block_numbers);
                         print(connected_block_numbers);
                       }
-
-
-
-
-
-
-
-
-
-
-
-
                     },
 
 
@@ -671,63 +659,3 @@ class DraggableImage {
   }
 
 }
-
-// void image_string() {
-//   List<int> keys = [4, 6, 7, 11, 12, 13, 23, 37];
-//   String inputString = connected_block_numbers;
-//   Map<int, List<int>> keyValueMap = {};
-//   List<String> connectedImages = inputString.split(',');
-//
-//   int currentIndex = 0;
-//   for (int i = 0; i < keys.length; i++) {
-//     int key = keys[i];
-//     List<int> values = [];
-//     while (currentIndex < connectedImages.length) {
-//       int value = int.parse(connectedImages[currentIndex]);
-//       if (value < key) {
-//         currentIndex++;
-//       } else if (value >= key && (i == keys.length - 1 || value < keys[i + 1])) {
-//         values.add(value);
-//         currentIndex++;
-//       } else {
-//         break;
-//       }
-//     }
-//
-//     if (values.isNotEmpty) {
-//       keyValueMap[key] = values;
-//     }
-//
-//   }
-//   BluetoothHelper.sendData(keyValueMap as String);
-//   print("hello");
-//   print(keyValueMap);
-// }
-
-// void main() {
-//   List<int> keys = [4, 6, 7, 11, 12, 13, 23, 37];
-//   String inputString = connected_block_numbers;
-//   Map<int, List<int>> keyValueMap = {};
-//   List<String> connectedImages = inputString.split(',');
-//   int startKey = 1;
-//   int endKey = 3;
-//
-//   int nextKey = (keys.length > 0) ? keys[1] : connectedImages.length - 1;
-//
-//   List<int> values = [];
-//   for (int j = 0; j < connectedImages.length; j++) {
-//     int value = int.parse(connectedImages[j]);
-//     if (value >= startKey && value < nextKey) {
-//       values.add(value);
-//     } else if (value == nextKey) {
-//       // 키값이 나타날 때까지의 값들을 리스트로 묶어줍니다.
-//       keyValueMap[nextKey] = values;
-//       values = [];
-//       startKey = nextKey;
-//       nextKey = (keys.length > 0) ? keys[0] : connectedImages.length + 1;
-//     }
-//   }
-//
-//   print(keyValueMap);
-//   BluetoothHelper.sendData(keyValueMap as String);
-// }
